70a2f208b8b5007bda3506def2a8c3fc5fed472e
Zaman Zulkarnaen <gavier.exe@gmail.com>
2025-05-21 21:22:28 +0700

    ini: Setup SDL3 as copied vendor

---
+++ Topaz/Topaz.cpp
@@ -0,0 +1,33 @@
+﻿#include "Topaz.h"
+
+int main(int argc, char* argv[])
+{
+  SDL_Window* window;
+  bool done = false;
+
+  SDL_Init(SDL_INIT_VIDEO);
+
+  window = SDL_CreateWindow("Topaz!", 640, 480, SDL_WINDOW_OPENGL);
+
+  if (window == NULL) {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+
+    return 1;
+  }
+
+  while (!done) {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        done = true;
+      }
+    }
+  }
+
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+
+  return 0;
+}
---
+++ Topaz/Topaz.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include <iostream>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
